b8ab04b754a2df212af4d10c4c301cdf270a2c92
Zynb246 <zeynebnecefli246@gmail.com>
2021-07-17 19:11:29 +0400

    Creating skills model for website

--- PortfolioWebsite/main/views.py
+++ PortfolioWebsite/main/views.py
@@ -1,6 +1,9 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from . models import Skill
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    skills = Skill.objects.all()
+    context = {'skills': skills}
+    return render(request, 'index.html',context)
